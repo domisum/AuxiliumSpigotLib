0a9864d62bde15fd2c85969d861894ff469ee82b
Dominik <dominik.weissenseel@gmx.de>
2017-07-07 15:32:40 +0200

    Reformatted AbstractPotionEffect

--- src/main/java/de/domisum/lib/auxiliumspigot/data/container/abstracts/AbstractPotionEffect.java
+++ src/main/java/de/domisum/lib/auxiliumspigot/data/container/abstracts/AbstractPotionEffect.java
@@ -12,30 +12,22 @@ public class AbstractPotionEffect
 {
 
 	// PROPERTIES
-	@SetByDeserialization
-	private String type;
-	@SetByDeserialization
-	private int amplifier;
-	@SetByDeserialization
-	private int duration;
+	@SetByDeserialization private String type;
+	@SetByDeserialization private int amplifier;
+	@SetByDeserialization private int duration;
 
 	// STATUS
 	private PotionEffectType type_;
 
 
-	// -------
 	// CONSTRUCTOR
-	// -------
-	@DeserializationNoArgsConstructor
-	public AbstractPotionEffect()
+	@DeserializationNoArgsConstructor public AbstractPotionEffect()
 	{
 
 	}
 
 
-	// -------
 	// GETTERS
-	// -------
 	private PotionEffectType getType()
 	{
 		if(this.type_ == null)
@@ -47,16 +39,15 @@ public class AbstractPotionEffect
 	private void determinePotionEffectType()
 	{
 		this.type_ = PotionEffectType.getByName(this.type);
+
 		if(this.type == null)
+			// noinspection ConstantConditions // type is not always null because of deserialized objects
 			throw new IllegalArgumentException("The potion effect type '"+this.type+"' does not exist");
 	}
 
 
-	// -------
 	// APPLICATION
-	// -------
-	@APIUsage
-	public void applyTo(PotionMeta potionMeta)
+	@APIUsage public void applyTo(PotionMeta potionMeta)
 	{
 		PotionEffect effect = new PotionEffect(getType(), this.duration, this.amplifier);
 		potionMeta.addCustomEffect(effect, true);
