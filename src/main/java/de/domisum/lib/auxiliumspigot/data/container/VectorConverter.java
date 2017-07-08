8b50389f399dae366914377c0041b83b402ec138
Dominik <dominik.weissenseel@gmx.de>
2017-07-08 14:43:18 +0200

    Added conversion to Location (VectorConverter)

--- src/main/java/de/domisum/lib/auxiliumspigot/data/container/VectorConverter.java
+++ src/main/java/de/domisum/lib/auxiliumspigot/data/container/VectorConverter.java
@@ -3,12 +3,14 @@ package de.domisum.lib.auxiliumspigot.data.container;
 import de.domisum.lib.auxilium.data.container.math.Vector3D;
 import de.domisum.lib.auxilium.util.java.annotations.APIUsage;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.util.Vector;
 
 @APIUsage
 public interface VectorConverter
 {
 
+	// VECTOR3D
 	@APIUsage static Vector3D toVector3D(Location location)
 	{
 		return new Vector3D(location.getX(), location.getY(), location.getZ());
@@ -17,6 +19,18 @@ public interface VectorConverter
 	@APIUsage static Vector3D toVector3D(Vector vector)
 	{
 		return new Vector3D(vector.getX(), vector.getY(), vector.getZ());
+	}
+
+
+	// LOCATION
+	@APIUsage static Location toLocation(Vector3D vector, World world)
+	{
+		return toLocation(vector, world, 0, 0);
+	}
+
+	@APIUsage static Location toLocation(Vector3D vector, World world, float yaw, float pitch)
+	{
+		return new Location(world, vector.x, vector.y, vector.z, yaw, pitch);
 	}
 
 }
