105c455e725e83fb79d706af9bfc27524277952b
Dominik <dominik.weissenseel@gmx.de>
2017-07-08 14:36:37 +0200

    Added Bukkit vector to Vector3D (VectorConverter)

--- src/main/java/de/domisum/lib/auxiliumspigot/data/container/VectorConverter.java
+++ src/main/java/de/domisum/lib/auxiliumspigot/data/container/VectorConverter.java
@@ -3,6 +3,7 @@ package de.domisum.lib.auxiliumspigot.data.container;
 import de.domisum.lib.auxilium.data.container.math.Vector3D;
 import de.domisum.lib.auxilium.util.java.annotations.APIUsage;
 import org.bukkit.Location;
+import org.bukkit.util.Vector;
 
 @APIUsage
 public interface VectorConverter
@@ -11,6 +12,11 @@ public interface VectorConverter
 	@APIUsage static Vector3D toVector3D(Location location)
 	{
 		return new Vector3D(location.getX(), location.getY(), location.getZ());
+	}
+
+	@APIUsage static Vector3D toVector3D(Vector vector)
+	{
+		return new Vector3D(vector.getX(), vector.getY(), vector.getZ());
 	}
 
 }
