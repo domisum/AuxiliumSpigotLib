6b1a9ace9074b96b761e7a79ef9206fa61e85de1
domisum <dominik.weissenseel@gmail.com>
2019-06-29 16:12:58 +0200

    Fixed action bar message sending

--- src/main/java/de/domisum/lib/auxiliumspigot/util/MessagingUtil.java
+++ src/main/java/de/domisum/lib/auxiliumspigot/util/MessagingUtil.java
@@ -1,25 +1,22 @@
 package de.domisum.lib.auxiliumspigot.util;
 
 import de.domisum.lib.auxilium.util.java.annotations.API;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import net.md_5.bungee.api.ChatColor;
-import net.minecraft.server.v1_13_R2.ChatMessageType;
-import net.minecraft.server.v1_13_R2.IChatBaseComponent;
-import net.minecraft.server.v1_13_R2.IChatBaseComponent.ChatSerializer;
-import net.minecraft.server.v1_13_R2.PacketPlayOutChat;
-import org.bukkit.craftbukkit.v1_13_R2.entity.CraftPlayer;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.entity.Player;
 
-public class MessagingUtil
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class MessagingUtil
 {
 
 	@API
 	public static void sendActionBarMessage(String message, Player... players)
 	{
-		IChatBaseComponent cbc = ChatSerializer.a("{\"text\": \""+message+"\"}");
-		PacketPlayOutChat ppoc = new PacketPlayOutChat(cbc, ChatMessageType.CHAT);
-
 		for(Player p : players)
-			((CraftPlayer) p).getHandle().playerConnection.sendPacket(ppoc);
+			p.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(message));
 	}
 
 
